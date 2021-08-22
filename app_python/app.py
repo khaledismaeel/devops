f6cca2f75e0385c0c3cb530a79d613f5e22395af
Khaled Ismaeel <k.ismaeel@innopolis.university>
2021-08-22 19:31:18 +0300

    Add minimalist time in Moscow functionality

--- app_python/app.py
+++ app_python/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    moscow_timezone = ZoneInfo('Europe/Moscow')
+    return datetime.now(tz=moscow_timezone).time().__str__()
 
 
 if __name__ == '__main__':
