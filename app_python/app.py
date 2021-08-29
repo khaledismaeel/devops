6d85f79044f0a89060fc9c1f6dc14e7eb805af9c
Khaled Ismaeel <k.ismaeel@innopolis.university>
2021-08-29 23:06:15 +0300

    Wrap app inside app factory

--- app_python/app.py
+++ app_python/app.py
@@ -2,14 +2,17 @@ from flask import Flask
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    moscow_timezone = ZoneInfo('Europe/Moscow')
-    return datetime.now(tz=moscow_timezone).time().__str__()
+    @app.route('/')
+    def time_in_moscow():
+        moscow_timezone = ZoneInfo('Europe/Moscow')
+        return datetime.now(tz=moscow_timezone).time().__str__()
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
