07f63838a62a2995ee7d1784ace1c8f127cb1bdd
Khaled Ismaeel <k.ismaeel@innopolis.university>
2021-10-04 00:18:17 +0300

    Add visits endpoint

--- app_python/test_app.py
+++ app_python/test_app.py
@@ -5,7 +5,7 @@ from datetime import time
 
 @pytest.fixture
 def client():
-    app = create_app()
+    app = create_app(reset=True)
     with app.test_client() as client:
         yield client
 
@@ -13,3 +13,10 @@ def client():
 def test_time_in_moscow_has_correct_format(client):
     response = client.get('/')
     time.fromisoformat(response.data.decode())
+
+
+def test_request_appends_time_to_file(client):
+    visits = client.get('/visits').data.split()
+    response = client.get('/').data
+    new_visits = client.get('/visits').data.split()
+    assert new_visits == visits + [response]
